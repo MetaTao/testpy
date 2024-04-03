1f6d3af06c9b918a9bea0972dc1df642f4afb811
nscq-shanbin <513654884@qq.com>
2024-04-03 10:22:14 +0800

    test git

--- test03.py
+++ test03.py
@@ -3,9 +3,12 @@ import unittest
 def add_numbers(x, y):
     return x + y
 
+print("1234")
+
 class TestAddNumbers(unittest.TestCase):
     def test_add_numbers(self):
         self.assertEqual(add_numbers(2, 3), 5)
 
 if __name__ == '__main__':
     unittest.main()
+
